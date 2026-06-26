b5dc8ffcffb63426a3185275aba3301f7818edd5
Dyllon Dunton <102751568+DyllonDunton1@users.noreply.github.com>
2026-06-26 16:28:29 -0400

    Expand shared profile config

--- src/config/profile.ts
+++ src/config/profile.ts
@@ -1,5 +1,12 @@
 export const PROFILE_IMAGE_PATH = '/portfolio_profile.jpg'
+export const PROFILE_IMAGE_ALT = 'Dyllon Dunton profile'
 export const RESUME_PATH = '/Dunton-Dyllon-Resume-June-2026.pdf'
+export const EMAIL = 'dyllon.dunton@maine.edu'
+export const LOCATION = 'Pittsfield, Maine (04967)'
+export const GITHUB_URL = 'https://github.com/DyllonDunton1'
+export const GITHUB_LABEL = 'DyllonDunton1'
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/dyllon-dunton-47ab8b1b8/'
+export const LINKEDIN_LABEL = 'dyllon-dunton'
 
 export const PROFILE_LINES = [
   'Robotics & Applied ML Engineer',
