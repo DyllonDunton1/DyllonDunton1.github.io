0a35b4aa3f9c0323cea6ecf485bd0ebb5e8198d1
Dyllon Dunton <102751568+DyllonDunton1@users.noreply.github.com>
2026-06-26 16:26:20 -0400

    Add shared profile constants

---
+++ src/config/profile.ts
@@ -0,0 +1,8 @@
+export const PROFILE_IMAGE_PATH = '/portfolio_profile.jpg'
+export const RESUME_PATH = '/Dunton-Dyllon-Resume-June-2026.pdf'
+
+export const PROFILE_LINES = [
+  'Robotics & Applied ML Engineer',
+  'M.S. Computer Engineering',
+  'University of Maine',
+]
