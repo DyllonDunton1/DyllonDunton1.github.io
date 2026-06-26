4cacac0181771b487c61b1f66d3f511dbcbb1a1a
Dyllon Dunton <102751568+DyllonDunton1@users.noreply.github.com>
2026-06-26 16:28:43 -0400

    Use shared profile config in main navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,16 @@
 import '../styles/Navbar.css'
+import {
+  EMAIL,
+  GITHUB_LABEL,
+  GITHUB_URL,
+  LINKEDIN_LABEL,
+  LINKEDIN_URL,
+  LOCATION,
+  PROFILE_IMAGE_ALT,
+  PROFILE_IMAGE_PATH,
+  PROFILE_LINES,
+  RESUME_PATH,
+} from '../config/profile'
 
 const Navbar = () => {
 /*
@@ -18,19 +30,19 @@ const Navbar = () => {
     <nav>
       <div className='identify'>
         <div className='mugshot'>
-            <img src="/portfolio_profile.jpg" className='mugshotImage' alt='Dyllon Dunton profile'></img>
+            <img src={PROFILE_IMAGE_PATH} className='mugshotImage' alt={PROFILE_IMAGE_ALT}></img>
         </div>
       </div>
       <div className='identify-text'>
         <h2 style={{ color: "black"}}>Dyllon Dunton</h2>
-        <p>Robotics & Applied ML Engineer</p>
-        <p>M.S. Computer Engineering</p>
-        <p style={{ marginBottom: '5%' }} >University of Maine</p>
-        <p><i className="fa-solid fa-envelope linkIcon" style={{paddingRight: '1%'}}/><span className="linker" onClick={() => {navigator.clipboard.writeText("dyllon.dunton@maine.edu");alert("Email copied!");}} >dyllon.dunton@maine.edu</span></p>
-        <p style={{ marginBottom: '5%' }}><i className="fa-solid fa-location-dot linkIcon" style={{paddingRight: '2%'}}/>Pittsfield, Maine (04967)</p>
-        <p><i className="fa-brands fa-github linkIcon" style={{paddingRight: '1%'}}/>{" "}<a href="https://github.com/DyllonDunton1" className='linker'>DyllonDunton1</a></p>
-        <p><i className="fa-brands fa-linkedin linkIcon" style={{paddingRight: '1%'}}/>{" "}<a href="https://www.linkedin.com/in/dyllon-dunton-47ab8b1b8/" className='linker'>dyllon-dunton</a></p>
-        <p><i className="fa-solid fa-file-arrow-down linkIcon" style={{paddingRight: '1%'}}/>{" "}<a href="/Dunton-Dyllon-Resume-June-2026.pdf" className='linker' download>Resume</a></p>
+        <p>{PROFILE_LINES[0]}</p>
+        <p>{PROFILE_LINES[1]}</p>
+        <p style={{ marginBottom: '5%' }} >{PROFILE_LINES[2]}</p>
+        <p><i className="fa-solid fa-envelope linkIcon" style={{paddingRight: '1%'}}/><span className="linker" onClick={() => {navigator.clipboard.writeText(EMAIL);alert("Email copied!");}} >{EMAIL}</span></p>
+        <p style={{ marginBottom: '5%' }}><i className="fa-solid fa-location-dot linkIcon" style={{paddingRight: '2%'}}/>{LOCATION}</p>
+        <p><i className="fa-brands fa-github linkIcon" style={{paddingRight: '1%'}}/>{" "}<a href={GITHUB_URL} className='linker'>{GITHUB_LABEL}</a></p>
+        <p><i className="fa-brands fa-linkedin linkIcon" style={{paddingRight: '1%'}}/>{" "}<a href={LINKEDIN_URL} className='linker'>{LINKEDIN_LABEL}</a></p>
+        <p><i className="fa-solid fa-file-arrow-down linkIcon" style={{paddingRight: '1%'}}/>{" "}<a href={RESUME_PATH} className='linker' download>Resume</a></p>
       </div>
 
   
