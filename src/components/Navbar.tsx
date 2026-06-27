3351a2740330336d500c9d956c4ea74d3010e977
Dyllon Dunton <102751568+DyllonDunton1@users.noreply.github.com>
2026-06-26 22:02:14 -0400

    Show actively learning in home navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import '../styles/Navbar.css'
 import { useMainScrollerWheel } from '../hooks/useMainScrollerWheel'
 import {
+  ACTIVELY_LEARNING,
   CURRENT_FOCUS,
   EMAIL,
   GITHUB_LABEL,
@@ -45,6 +46,10 @@ const Navbar = () => {
         <div className='currentFocus'>
           <p className='currentFocusLabel'>Current Focus</p>
           <p>{CURRENT_FOCUS.join(' · ')}</p>
+        </div>
+        <div className='currentFocus'>
+          <p className='currentFocusLabel'>Actively Learning</p>
+          <p>{ACTIVELY_LEARNING.join(' · ')}</p>
         </div>
         <p><i className="fa-solid fa-envelope linkIcon" style={{paddingRight: '1%'}}/><span className="linker" onClick={() => {navigator.clipboard.writeText(EMAIL);alert("Email copied!");}} >{EMAIL}</span></p>
         <p style={{ marginBottom: '5%' }}><i className="fa-solid fa-location-dot linkIcon" style={{paddingRight: '2%'}}/>{LOCATION}</p>
